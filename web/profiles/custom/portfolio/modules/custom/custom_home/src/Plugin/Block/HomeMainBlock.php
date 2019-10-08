3f987d2be667023688e6bf17fc4a9c99010b8d28
Romain Trenet <romain.trenet@gmail.com>
2019-10-08 19:23:34 +0200

    Add correct font, settings for typey fonts

--- web/profiles/custom/portfolio/modules/custom/custom_home/src/Plugin/Block/HomeMainBlock.php
+++ web/profiles/custom/portfolio/modules/custom/custom_home/src/Plugin/Block/HomeMainBlock.php
@@ -22,7 +22,7 @@ class HomeMainBlock extends BlockBase {
   public function build() {
     // @todo : admin page for elements.
     $image_path = drupal_get_path('module', 'custom_home') . '/img/fond_degrade.jpg';
-    $title = 'Développeur Drupal &<br>Intégrateur';
+    $title = 'Développeur Drupal &<br>Intégrateur.';
 
     return [
       '#theme' => 'home_main',
