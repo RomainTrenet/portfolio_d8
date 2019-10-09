ad6f0189ac12ee5d369fad63e7d8bab9d33cdb7c
Romain Trenet <romain.trenet@gmail.com>
2019-10-09 19:29:21 +0200

    Button almost ok.

--- web/profiles/custom/portfolio/modules/custom/custom_home/src/Plugin/Block/HomeMainBlock.php
+++ web/profiles/custom/portfolio/modules/custom/custom_home/src/Plugin/Block/HomeMainBlock.php
@@ -23,7 +23,7 @@ class HomeMainBlock extends BlockBase {
     // @todo : admin page for elements.
     $image_path = drupal_get_path('module', 'custom_home') . '/img/fond_degrade.jpg';
     $title = 'Développeur Drupal &<br>Intégrateur.';
-    $ctas = '<a href="#" class="btn-main">CONTACT</a><a href="#" class="btn-second">Mon C.V.</a><br>';
+    $ctas = '<a href="#" class="btn-main btn-white">CONTACT</a><a href="#" class="btn-second btn-white">Mon C.V.</a><br>';
     $ctas .= '<a href="#" class="btn-third">THIRD</a><a href="#" class="btn-fourth">Fourth.</a>';
 
     return [
