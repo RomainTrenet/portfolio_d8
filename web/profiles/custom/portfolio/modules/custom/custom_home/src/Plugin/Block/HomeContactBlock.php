16c3780b6d0d3b66375bd3fc509741bf05bb1170
Romain Trenet <romain.trenet@gmail.com>
2020-02-18 17:56:01 +0100

    Export block types to test import in prod ; work in progress for Home configurable block

--- web/profiles/custom/portfolio/modules/custom/custom_home/src/Plugin/Block/HomeContactBlock.php
+++ web/profiles/custom/portfolio/modules/custom/custom_home/src/Plugin/Block/HomeContactBlock.php
@@ -8,9 +8,9 @@ use Drupal\Core\Block\BlockBase;
  * Provides the home contact form block.
  *
  * @Block(
- *   id = "home_contact_block",
- *   admin_label = @Translation("Home contact block"),
- *   category = @Translation("Custom"),
+ *     id="home_contact_block",
+ *     admin_label=@Translation("Home contact block"),
+ *     category=@Translation("Custom"),
  * )
  */
 class HomeContactBlock extends BlockBase {
